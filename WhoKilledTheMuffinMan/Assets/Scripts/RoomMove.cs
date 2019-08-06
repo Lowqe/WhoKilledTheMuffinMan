a40708275b4ece71a76034a477389fca7b928e76
Lowqe <Lowqe@users.noreply.github.com>
2019-08-06 17:25:14 +0200

    Movement and camera

---
+++ WhoKilledTheMuffinMan/Assets/Scripts/RoomMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+
+    public Vector2 cameraChange;
+    public Vector2 playerChange;
+    private CameraMovement cam;
+
+    void Start()
+    {
+       // cam = Camera.Main.GetComponent<CameraMovement>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
+
+}
